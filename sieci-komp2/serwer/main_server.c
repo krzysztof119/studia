ade56650e147b5963ba1149fef5569f8719027a1
krzysztof119 <67243080+krzysztof119@users.noreply.github.com>
2024-01-26 23:47:02 +0100

    Update main_server.c

--- sieci-komp2/serwer/main_server.c
+++ sieci-komp2/serwer/main_server.c
@@ -19,7 +19,7 @@
 #define PORT 1234
 #define MAX_SIZE_PER_ROOM 2
 #define BUF_SIZE_TCP 32768
-#define BUF_SIZE_UDP 2<<13
+#define BUF_SIZE_UDP 8192
 #define SECONDS_TO_WAIT 5
 #define POSSIBLE_ATTEMPTS 4
 #define CONNECTION_TEST_MESSAGE (char*)"test"
@@ -31,64 +31,72 @@ unsigned int cCount = 0;
 
 void childend(int signo) {wait(NULL);}
 
-struct Cln{
+struct ClnTCP{
     int cfd;
     struct sockaddr_in caddr;
-};struct Cln *clnArray[MAX_SIZE_PER_ROOM] = {};
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+};struct ClnTCP *clnArrayTCP[MAX_SIZE_PER_ROOM] = {};
+pthread_mutex_t mutexTCP = PTHREAD_MUTEX_INITIALIZER;
 
-void answer(struct Cln *cln,void* buf, int rc){
-    pthread_mutex_lock(&mutex);
+struct ClnUDP{
+    int fd;
+    socklen_t sl;
+    struct sockaddr_in ca;
+};
+pthread_mutex_t mutexUDP = PTHREAD_MUTEX_INITIALIZER;
+
+void answer(struct ClnTCP *clnTCP,void* buf, int rc){
+    pthread_mutex_lock(&mutexTCP);
     
     for(int i = 0; i < MAX_SIZE_PER_ROOM; i++){
-        if(!clnArray[i] || clnArray[i] == cln)
+        if(!clnArrayTCP[i] || clnArrayTCP[i] == clnTCP)
             continue;
 
         int wr = 0;
         while(wr < rc){
-            wr += write(clnArray[i]->cfd, buf + wr, rc - wr);
+            wr += write(clnArrayTCP[i]->cfd, buf + wr, rc - wr);
         }
     }
-    pthread_mutex_unlock(&mutex);
+
+    pthread_mutex_unlock(&mutexTCP);
 }
 
-void addNewClient(struct Cln *cln){
-    pthread_mutex_lock(&mutex);
+void addNewClient(struct ClnTCP *clnTCP){
+    pthread_mutex_lock(&mutexTCP);
 
     for(int i = 0; i < MAX_SIZE_PER_ROOM; i++){  
-        if(clnArray[i])
+        if(clnArrayTCP[i])
             continue;
 
-        clnArray[i] = cln;
+        clnArrayTCP[i] = clnTCP;
         break;
     }
     
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutexTCP);
 }
 
-void removeClient(struct Cln *cln){
-    pthread_mutex_lock(&mutex);
+void removeClient(struct ClnTCP *clnTCP){
+    pthread_mutex_lock(&mutexTCP);
 
     for(int i = 0; i < MAX_SIZE_PER_ROOM; i++){
-        if(!clnArray[i] || clnArray[i] != cln)
+        if(!clnArrayTCP[i] || clnArrayTCP[i] != clnTCP)
             continue;
 
-        clnArray[i] = NULL;
+        clnArrayTCP[i] = NULL;
         break;
     }
     
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutexTCP);
 }
 
 #if defined(PROTOTYPE_CHECK_ACTIVITY)
-int connectionCheck(struct Cln *cln, void *buf, int replySize, int bytesAviable, int retryCount, int *bytesDeleted){
+int connectionCheck(struct ClnTCP *clnTCP, void *buf, int replySize, int bytesAviable, int retryCount, int *bytesDeleted){
     int i = 1, rc = 0;
     char *testMessage;
     *bytesDeleted = 0;
 
     for(i; i <= replySize; i++){
         do{
-            rc += read(cln->cfd, buf + rc, replySize - rc);
+            rc += read(clnTCP->cfd, buf + rc, replySize - rc);
         }while(rc < replySize);
         *bytesDeleted += rc;
 
@@ -104,8 +112,8 @@ int connectionCheck(struct Cln *cln, void *buf, int replySize, int bytesAviable,
 }
 #endif
 
-void *cHandle(void *arg){
-    struct Cln *cln = (struct Cln*) arg;
+void *cHandleTCP(void *arg){
+    struct ClnTCP *clnTCP = (struct ClnTCP*) arg;
     void *buf = malloc(BUF_SIZE_TCP);
     
     char end;
@@ -120,7 +128,7 @@ void *cHandle(void *arg){
     double elapsedTime;
 #endif
 
-    addNewClient(cln);
+    addNewClient(clnTCP);
     cCount++;
 
     while(1){
@@ -130,7 +138,7 @@ void *cHandle(void *arg){
 #endif
 
         while(1){
-            ioctl(cln->cfd, FIONREAD, &bytesAviable);
+            ioctl(clnTCP->cfd, FIONREAD, &bytesAviable);
 
 #if !defined(PROTOTYPE_CHECK_ACTIVITY)
             if(bytesAviable > 0){
@@ -150,7 +158,7 @@ void *cHandle(void *arg){
             
             if(elapsedTime >= SECONDS_TO_WAIT){
                 retryCount++;
-                (void) !write(cln->cfd, CONNECTION_TEST_MESSAGE, replySize);
+                (void) !write(clnTCP->cfd, CONNECTION_TEST_MESSAGE, replySize);
             }
 #endif
             sleep(SERVER_FREQUENCY);
@@ -158,7 +166,7 @@ void *cHandle(void *arg){
 
 #if defined(PROTOTYPE_CHECK_ACTIVITY)
         if(bytesAviable == 0 && retryCount >= POSSIBLE_ATTEMPTS){
-            removeClient(cln);
+            removeClient(clnTCP);
             break;
         }
 
@@ -169,7 +177,7 @@ void *cHandle(void *arg){
         if(retryCount == 0){
             rc = 0;
         } else {
-            rc = connectionCheck(cln, buf, replySize, bytesAviable, retryCount, &bytesDeleted);
+            rc = connectionCheck(clnTCP, buf, replySize, bytesAviable, retryCount, &bytesDeleted);
             if(rc == 0) { //informacja zwrotna od klienta, bez wiadomosci
                 continue;
             }
@@ -180,19 +188,19 @@ void *cHandle(void *arg){
         rc = 0;
 #endif
         while(1){
-            rc += read(cln->cfd, buf + rc, BUF_SIZE_TCP - rc);
+            rc += read(clnTCP->cfd, buf + rc, BUF_SIZE_TCP - rc);
             //(void) !write(1, buf, sizeof(buf));
             end = ((char*)buf)[rc-1];
 
 #if !defined(PROTOTYPE_CHECK_ACTIVITY)
             if(rc == BUF_SIZE_TCP){
-                answer(cln, buf, rc);
+                answer(clnTCP, buf, rc);
                 break;
             }
 
             if (rc >= bytesAviable || end == END_PACKAGE_CHARACTER){    
                 if(rc != BUF_SIZE_TCP){
-                    answer(cln, buf, rc);
+                    answer(clnTCP, buf, rc);
                 }
                 break;
             }
@@ -200,7 +208,7 @@ void *cHandle(void *arg){
             
 #if defined(PROTOTYPE_CHECK_ACTIVITY)
             if(rc == BUF_SIZE_TCP || rc == bytesAviable - bytesDeleted){
-                answer(cln, buf, rc);
+                answer(clnTCP, buf, rc);
                 rc = 0;
             }
 
@@ -212,7 +220,7 @@ void *cHandle(void *arg){
 #endif
         } 
     }
-    close(cln->cfd); free(cln); free(buf);
+    close(clnTCP->cfd); free(clnTCP); free(buf);
     cCount--;
     pthread_detach(pthread_self());
 }
@@ -223,7 +231,7 @@ void serverTCP(){
     struct sockaddr_in saddr, caddr;
 
     signal(SIGCHLD, childend);
-    setsockopt(cfd,SOL_SOCKET,SO_REUSEADDR, (int*) 1,sizeof(int));
+    //setsockopt(cfd,SOL_SOCKET,SO_REUSEADDR, (int*) 1,sizeof(int));
     
     saddr.sin_family = AF_INET;
     saddr.sin_addr.s_addr = INADDR_ANY;
@@ -251,40 +259,73 @@ void serverTCP(){
         }
         printf("Nowe poloczenie %d\n", cfd);
 
-        struct Cln *cln = (struct Cln*) malloc(sizeof(struct Cln));
-        cln->cfd = cfd;
-        cln->caddr = caddr;
+        struct ClnTCP *clnTCP = (struct ClnTCP*) malloc(sizeof(struct ClnTCP));
+        clnTCP->cfd = cfd;
+        clnTCP->caddr = caddr;
 
-        pthread_create(&pThread, NULL, &cHandle, (void*) cln);
+        pthread_create(&pThread, NULL, &cHandleTCP, (void*) clnTCP);
 
     }close(sfd);
-    free(clnArray);
+    free(clnArrayTCP);
+}
+
+void *cHandleUDP(void *arg){
+    struct ClnUDP *clnUDP = (struct ClnUDP*) arg;
+    int rc;
+    void *buf = malloc(BUF_SIZE_TCP);
+
+    rc = recvfrom(clnUDP->fd, buf, sizeof(buf), 0, (struct sockaddr*)&clnUDP->ca, &clnUDP->sl);
+    //(void) !write(1, buf, rc);
+    pthread_mutex_lock(&mutexUDP);
+
+    for(int i = 0; i < MAX_SIZE_PER_ROOM; i++){
+        if(!clnArrayTCP[i]) // && clnArrayTCP[i]->caddr.sin_port == clnUDP->ca.sin_port
+            continue;
+
+        int wr = 0;
+        while(wr < rc){
+            sendto(clnUDP->fd, buf, rc, 0, (struct sockaddr*)&clnUDP->ca, clnUDP->sl);
+        }
+    }
+    
+    pthread_mutex_lock(&mutexUDP);
 }
 
 void serverUDP(){
-    int fd, rc;
+    pthread_t pThread;
+    int fd;
     socklen_t sl;
-    void *buf = malloc(BUF_SIZE_TCP);
+
+    signal(SIGCHLD, childend);
+
     struct sockaddr_in sa, ca;
 
     sa.sin_family = AF_INET;
     sa.sin_addr.s_addr = INADDR_ANY;
-    sa.sin_port = htons(1234);
+    sa.sin_port = htons(1235);
     fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
     bind(fd, (struct sockaddr*)&sa, sizeof(sa));
     while(1) {
-    sl = sizeof(ca);
-    rc = recvfrom(fd, buf, sizeof(buf), 0,
-    (struct sockaddr*)&ca, &sl);
-    (void) !write(1, buf, rc);
+        sl = sizeof(ca);
+
+        //printf("Nowe poloczenie %d\n", fd);
+
+        struct ClnUDP *clnUDP = (struct ClnUDP*) malloc(sizeof(struct ClnUDP));
+        clnUDP->fd = fd;
+        clnUDP->sl = sl;
+        clnUDP->ca = ca;
+
+        pthread_create(&pThread, NULL, &cHandleUDP, (void*) clnUDP);
     
-    sendto(fd, buf, rc, 0, (struct sockaddr*)&ca, sl);
     }
 }
 
 int main(int argc, char** argv){
-
+    if(fork() == 0){
         serverTCP();
+    } else{
+        serverUDP();
+    }
     
     return EXIT_SUCCESS;
 }
